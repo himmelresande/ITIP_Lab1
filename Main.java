caff63ea670ae50ec339ff482811be4b1be8d853
himmelresande <119213654+himmelresande@users.noreply.github.com>
2023-09-26 13:31:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,11 +16,8 @@ public class Main {
         if (number <= 3) {
             return true;
         }
-        if (number % 2 == 0 || number % 3 == 0) {
-            return false;
-        }
-        for (int i = 5; i * i <= number; i += 6) {
-            if (number % i == 0 || number % (i + 2) == 0) {
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
